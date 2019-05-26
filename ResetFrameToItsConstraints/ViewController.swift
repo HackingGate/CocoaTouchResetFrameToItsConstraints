9b55183c45ebb443aa76493f8b89b09a871f0192
Dong <donly@magicalboy.com>
2019-05-26 23:53:20 +0800

    another way to slove

--- ResetFrameToItsConstraints/ViewController.swift
+++ ResetFrameToItsConstraints/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var buttonCenterXCons: NSLayoutConstraint!
     @IBOutlet weak var centerButton: UIButton!
 
     override func viewDidLoad() {
@@ -17,13 +18,19 @@ class ViewController: UIViewController {
     }
     
     @IBAction func centerButtonClicked(_ sender: UIButton) {
-        
+        for con in sender.constraints {
+            con.isActive = true
+            print("con=\(con)")
+        }
         UIView.animate(withDuration: 1.5, animations: {
             sender.frame.origin.y = 20.0
+            self.buttonCenterXCons.isActive = false
         }) { (completed) in
             // TODO: return to its original positon use constrains
             // Trying to solve https://stackoverflow.com/q/48291941/4063462
-            
+            // Donnie's implement, or another way to slove
+            self.buttonCenterXCons.isActive = true
+            self.centerButton.setNeedsLayout()
         }
         
     }
